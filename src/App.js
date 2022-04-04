f0e244763556a38a27d949b26f6da1f3e19dc0d2
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2022-04-04 16:52:32 +0100

    Added API(not working) and changed src/index.js to remove reactdom error

--- src/App.js
+++ src/App.js
@@ -1,50 +1,68 @@
 import './App.css';
-import { Page, H1, BackLink } from 'govuk-react';
-import { Paragraph } from 'govuk-react';
-import { OrderedList, ListItem } from 'govuk-react';
-import { Button, ButtonArrow } from 'govuk-react';
-import { Link } from 'govuk-react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
 
+// import { Page, H1, BackLink } from 'govuk-react';
+// import { Paragraph } from 'govuk-react';
+// import { OrderedList, ListItem } from 'govuk-react';
+// import { Button, ButtonArrow } from 'govuk-react';
+//const axios = require('axios').default;
+const baseURL = "https://dvla-backend.herokuapp.com/api/clientsdata";
 
-function App() {
+export default function App() {
+  const [post, setPost] = React.useState(null);
+
+  React.useEffect(() => {
+    axios.get(baseURL).then((response) => {
+      setPost(response.data);
+    });
+  }, []);
+
+  if (!post) return "No post!";
+
   return (
-    <div className="App">
-      <Page beforeChildren={<BackLink href="#">Back</BackLink>}>
-        <H1>
-          Book your theory test here
-        </H1>
-      
-        <Paragraph>
-          Book a theory test for a specific date:
-        </Paragraph>
-
-        <Button
-          icon={<ButtonArrow />}
-          start
-        >
-          Start now
-        </Button>
-
-        <Paragraph>
-          Cancel or rechedule your booked test using a reference number from: 
-          <a href='#'Cancel appointment/>
-        </Paragraph>
-
-        <Link href="#">
-          Link example
-        </Link>
-        
-        <OrderedList listStyleType="bullet">
-          <ListItem>
-            A recent reminder letter from DVLA.
-          </ListItem>
-          <ListItem>
-            Confirmation e-mail from DVLA.
-          </ListItem>
-        </OrderedList>
-      </Page>
+    <div>
+      <h1>{post}</h1>
     </div>
   );
 }
 
-export default App;
+
+// function App() {
+//   return (
+    // <div className="App">
+    //   <Page beforeChildren={<BackLink href="#">Back</BackLink>}>
+    //     <H1>
+    //       Book your theory test here
+    //     </H1>
+      
+    //     <Paragraph>
+    //       Book a theory test for a specific date:
+    //     </Paragraph>
+
+    //     <Button
+    //       icon={<ButtonArrow />}
+    //       start
+    //     >
+    //       Start now
+    //     </Button>
+
+
+    //     <Paragraph>
+    //       [Cancel](https://en.wikipedia.org/wiki/Markdown) or [rechedule](https://en.wikipedia.org/wiki/Markdown) your booked test using a reference number from:
+    //     </Paragraph>
+
+    //     <OrderedList listStyleType="bullet">
+    //       <ListItem>
+    //         A recent reminder letter from DVLA.
+    //       </ListItem>
+    //       <ListItem>
+    //         Confirmation e-mail from DVLA.
+    //       </ListItem>
+    //     </OrderedList>
+    //   </Page>
+    // </div>
+//   );
+// }
+
+// export default App;
