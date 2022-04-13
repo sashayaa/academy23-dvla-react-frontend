b9a3ae5e9fbc714706a32bcf1b59f5f640bf380a
Anne Figer <anne-laure.figer@madetech.com>
2022-04-13 12:17:30 +0100

    added Panel component to confirmation screen

--- src/Pages/Confirmation.js
+++ src/Pages/Confirmation.js
@@ -1,19 +1,27 @@
 import React, { useEffect } from "react";
 import { Page, H2, UnorderedList, H4, H3 } from "govuk-react";
 import "../Styles/Home.css";
-import { Paragraph } from "govuk-react";
+import { Paragraph, Panel } from "govuk-react";
 import { OrderedList, ListItem } from "govuk-react";
 import { Button, ButtonArrow } from "govuk-react";
 import { Footer } from "govuk-react";
 import { Link } from "react-router-dom";
 
 export default function Confirmation() {
-    useEffect(() => {
-      window.scroll(0, 0);
-    }, []);
+  useEffect(() => {
+    window.scroll(0, 0);
+  }, []);
 
 
-    return(
-        <div>THIS (might have) WORKED!</div>
-    )
+  return (
+
+    <Panel title="Application complete">
+      Your reference number
+      <br />
+      <strong>
+        Random
+      </strong>
+    </Panel>
+
+  )
 }
