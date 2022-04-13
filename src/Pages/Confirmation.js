e42d53c1150ab1f429e43cc76c20c94e44a5d2fe
Aleksandra Ivanova <aleksandra98iv@gmail.com>
2022-04-13 14:41:06 +0100

    Ajusted confirmation panel to the center on Confirmation Page

--- src/Pages/Confirmation.js
+++ src/Pages/Confirmation.js
@@ -1,10 +1,10 @@
 import React, { useEffect } from "react";
 import { Page, H2, UnorderedList, H4, H3 } from "govuk-react";
-import "../Styles/Home.css";
+import "../Styles/Confirmation.css";
 import { Paragraph, Panel } from "govuk-react";
 import { OrderedList, ListItem } from "govuk-react";
 import { Button, ButtonArrow } from "govuk-react";
-import { Footer } from "govuk-react";
+import { Footer, TopNav } from "govuk-react";
 import { Link } from "react-router-dom";
 
 export default function Confirmation() {
@@ -15,13 +15,46 @@ export default function Confirmation() {
 
   return (
 
-    <Panel title="Application complete">
-      Your reference number
-      <br />
-      <strong>
-        Random
-      </strong>
-    </Panel>
+    <div className="ConfirmationPage">
 
+      <TopNav
+        company={
+          <TopNav.Anchor href="localhost:3000" target="new">
+            <TopNav.IconTitle /*icon={<Crown height="32" width="36"/>}*/>
+              GOV.UK
+            </TopNav.IconTitle>
+          </TopNav.Anchor>
+        }
+        /*serviceTitle={<TopNav.NavLink href="https://example.com" target="new">Service Title</TopNav.NavLink>}*/
+      />
+
+      <div className="Container">
+        <div className="Panel">
+          <Panel title="Application complete">
+            Your reference number
+            <br />
+            <strong>
+              Random
+            </strong>
+          </Panel>
+        </div>
+        <div className="Info">
+          <Paragraph>
+            Shortly you will get an e-mail with your reference number and all of the information regarding your test appointment.
+          </Paragraph>
+        </div>
+      </div>
+
+      <div className="Footer">
+        <Footer
+          meta={
+            <Footer.MetaCustom>
+              Built by the{" "}
+              <Footer.Link href="/">Government Digital Service</Footer.Link>
+            </Footer.MetaCustom>
+          }
+        />
+      </div>
+    </div>
   )
 }
